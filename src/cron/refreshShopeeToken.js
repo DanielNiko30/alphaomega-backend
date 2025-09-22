c2ad019c18c6fddc27059f4aa298dd83134e9130
DanielNiko30 <danielniko300703@gmail.com>
2025-09-23 00:35:39 +0700

    index

--- src/cron/refreshShopeeToken.js
+++ src/cron/refreshShopeeToken.js
@@ -2,28 +2,19 @@ const cron = require("node-cron");
 const { Shopee } = require("../model/shopee_model");
 const { isTokenExpired, refreshShopeeToken } = require("../utils/shopee_helper");
 
-/**
- * CRON untuk cek token Shopee
- * - Setiap 4 jam akan dicek otomatis
- * - Jika expired, langsung refresh token
- */
 cron.schedule("0 */4 * * *", async () => {
     console.log(`[CRON] Cek token Shopee... ${new Date().toISOString()}`);
-
     try {
         const shops = await Shopee.findAll();
-
         for (let shop of shops) {
-            console.log(`[CRON] Shop ID ${shop.shop_id} | last_updated: ${shop.last_updated}, expire_in: ${shop.expire_in}`);
-
             if (isTokenExpired(shop)) {
-                console.log(`[CRON] Token expired! Mulai refresh...`);
+                console.log(`[CRON] Token expired! Refreshing...`);
                 await refreshShopeeToken(shop);
             } else {
                 console.log(`[CRON] Token masih aktif ✅`);
             }
         }
     } catch (err) {
-        console.error(`[CRON ERROR]`, err.message);
+        console.error("[CRON ERROR]", err.message);
     }
 });
