bdd64b36a9bdfee56294ff733a4e76c4bd440254
DanielNiko30 <danielniko300703@gmail.com>
2025-10-01 13:54:05 +0700

    index

--- src/controller/lazada_controller.js
+++ src/controller/lazada_controller.js
@@ -216,7 +216,8 @@ const createProductLazada = async (req, res) => {
 
         const apiPath = "/product/create";
         // Timestamp Lazada harus dalam detik GMT
-        const timestamp = Math.floor(Date.now() / 1000 - new Date().getTimezoneOffset() * 60).toString();
+        const timestamp = Math.floor(Date.now() / 1000).toString();
+
         const signParams = {
             app_key: process.env.LAZADA_APP_KEY,
             access_token: lazadaData.access_token,
