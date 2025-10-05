57ba26964b6bbbadae03e1389d32603048250c1d
DanielNiko30 <danielniko300703@gmail.com>
2025-10-05 14:16:29 +0700

    index

--- src/controller/lazada_controller.js
+++ src/controller/lazada_controller.js
@@ -109,13 +109,11 @@ const createDummyProduct = async (req, res) => {
                         brand: "No Brand",
                         description: "Tas Tote Bag Wanita (Canvas) untuk percobaan API Lazada.",
                         short_description: "Tote Bag Kanvas API Test.",
-
-                        // ✅ ubah semua dari array → string (karena kategori 17935 tidak mendukung array untuk CPV)
-                        Bag_Size: "60766",    // Medium
-                        material: "30716",    // Canvas
-                        Bag_type: "84749"     // Tote Bag
+                        Bag_Size: "60766",
+                        material: "30716",
+                        Bag_type: "120147845" // ✅ ID CPV yang valid untuk "Tote Bag"
                     },
-                    Skus: {
+                    s: {
                         Sku: [{
                             SellerSku: "SKU-TOTE-" + uniqueSuffix, // SKU baru
                             // Mengubah nilai numerik menjadi tipe Number (tanpa kutip)
