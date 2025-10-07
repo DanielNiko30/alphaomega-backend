fca5ef4d0f5279ac04b02d05be889a3d6d36deaa
DanielNiko30 <danielniko300703@gmail.com>
2025-10-07 17:05:46 +0700

    index

--- src/controller/lazada_controller.js
+++ src/controller/lazada_controller.js
@@ -638,7 +638,7 @@ const createDummyProduct = async (req, res) => {
                         short_description: "Tote Bag Kanvas API Test.",
                         material: "28232", // Canvas
                         Net_Weight: 500,
-                        unit_metric: "5450"
+                        unit_metric: "G"
                     },
                     Skus: {
                         Sku: [
