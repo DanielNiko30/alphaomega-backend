d01b61eeda83029e4955309d46d79a25108e3279
DanielNiko30 <danielniko300703@gmail.com>
2025-10-21 17:50:00 +0700

    index

--- src/config/fcm.js
+++ src/config/fcm.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
+const serviceAccountPath = path.join(__dirname, "./src/config/serviceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(require(serviceAccountPath)),
