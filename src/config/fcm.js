e9c171ad9604685563023e1f09d0cb52f035f5b2
DanielNiko30 <danielniko300703@gmail.com>
2025-10-21 17:58:21 +0700

    index

--- src/config/fcm.js
+++ src/config/fcm.js
@@ -1,11 +1,18 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
-const serviceAccountPath = "/home/alphaomega2/alphaomega-backend/src/config/serviceAccountKey.json";
+// Path absolut ke serviceAccountKey.json
+const serviceAccountPath = path.resolve(
+  __dirname,
+  "serviceAccountKey.json"
+);
 
+// Load file JSON
 const serviceAccount = require(serviceAccountPath);
 
+// Inisialisasi Firebase Admin
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
