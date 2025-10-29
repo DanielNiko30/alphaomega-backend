991ac384c7a108bd4061bbc18810aae06d30d150
DanielNiko30 <danielniko300703@gmail.com>
2025-10-29 13:23:56 +0700

    index

--- src/controller/lazada_controller.js
+++ src/controller/lazada_controller.js
@@ -1517,6 +1517,16 @@ const aturPickup = async (req, res) => {
     }
 };
 
+function canonicalize(obj) {
+    if (Array.isArray(obj)) return `[${obj.map(canonicalize).join(",")}]`;
+    else if (obj && typeof obj === "object") {
+        const keys = Object.keys(obj).sort();
+        return `{${keys.map(k => `"${k}":${canonicalize(obj[k])}`).join(",")}}`;
+    } else if (typeof obj === "string") return `"${obj}"`;
+    else if (typeof obj === "boolean" || typeof obj === "number") return String(obj);
+    else return "null";
+}
+
 function makeSign(apiPath, sysParams, bodyToSign, appSecret) {
     const sortedKeys = Object.keys(sysParams).sort();
     let baseStr = apiPath;
@@ -1524,7 +1534,7 @@ function makeSign(apiPath, sysParams, bodyToSign, appSecret) {
         const v = sysParams[k];
         baseStr += k + v;
     }
-    const bodyStr = bodyToSign === undefined ? "" : JSON.stringify(bodyToSign);
+    const bodyStr = canonicalize({ getDocumentReq });
     baseStr += bodyStr;
     const sign = crypto.createHmac("sha256", appSecret).update(baseStr, "utf8").digest("hex").toUpperCase();
     return { sign, baseStr, bodyStr };
