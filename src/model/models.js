8ce3d31a3f721c4a100fd403679323cd095520ca
DanielNiko30 <danielniko300703@gmail.com>
2025-11-17 16:45:16 +0700

    index

--- src/model/models.js
+++ src/model/models.js
@@ -11,6 +11,16 @@ const User = db.define('user', {
     name: { type: DataTypes.STRING(255), allowNull: false },
     role: { type: DataTypes.STRING(20), allowNull: false },
     no_telp: { type: DataTypes.STRING(50), allowNull: false },
+    jenis_kelamin: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        defaultValue: "tidak diketahui",
+    },
+    alamat: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        defaultValue: "tidak diketahui",
+    },
 }, { tableName: 'user', timestamps: false });
 
 // Kategori
